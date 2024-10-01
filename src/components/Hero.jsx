9ec558246328eb8dc607dac7ba1ec54705432757
mafas7647 <mafas7647@gmail.com>
2024-10-01 15:36:54 +0530

    mafas-portforlio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,28 +35,33 @@ const Hero = () => {
                         <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                             {HERO_CONTENT}
                         </p>
-        
-            <div className="m-1 flex items-center justify-center gap-6 text-3xl">
-                <a href="https://www.linkedin.com/in/m-mafas/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIN">
-                    <FaLinkedin />
-                </a>
-                <a href="https://github.com/MafasDevGitHub" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-                    <FaGithub />
-                </a>
-                <a href="https://www.instagram.com/mafas_official_11/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-                    <FaInstagram />
-                </a>
-                <a href="https://www.facebook.com/mhdmafas.mhdmafas.3" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-                    <FaFacebook />
-                </a>
-            </div><br />
-                        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 tex-sm text-stone-800 mb-10">
+
+                        <div className="m-1 flex items-center justify-center gap-6 text-3xl">
+                            <a href="https://www.linkedin.com/in/m-mafas/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIN">
+                                <FaLinkedin />
+                            </a>
+                            <a href="https://github.com/MafasDevGitHub" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                                <FaGithub />
+                            </a>
+                            <a href="https://www.instagram.com/mafas_official_11/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                                <FaInstagram />
+                            </a>
+                            <a href="https://www.facebook.com/mhdmafas.mhdmafas.3" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                                <FaFacebook />
+                            </a>
+                        </div><br />
+                        <a href="/mafas-portforlio/resume.pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            download="Mafas_resume.pdf"
+                            className="bg-white rounded-full p-4 tex-sm text-stone-800 mb-10">
                             Download Resume
                         </a>
+
                     </div>
                 </div>
             </div>
-        </motion.div>
+        </motion.div >
     );
 };
 
